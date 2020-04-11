d38420d4c93aed7a96ee802f6cb048d1db913dd2
HU <huxixi.love.story@gmail.com>
2020-04-11 14:08:45 +0800

    Update stl_iterator.h

--- Part X A Deep Understanding of STL/2. Iterators & Traits/stl_iterator.h
+++ Part X A Deep Understanding of STL/2. Iterators & Traits/stl_iterator.h
@@ -3,7 +3,7 @@ struct input_iterator_tag { };
 struct output_iterator_tag { };
 struct forward_iterator_tag : public input_iterator_tag { };
 struct bidirectional_iterator_tag : public forward_iterator_tag { };
-struct random_access_iterator_tag : bidirectional_iterator_tag { };
+struct random_access_iterator_tag : public bidirectional_iterator_tag { };
 
 // stl::iterator class
 template <class Category, 
@@ -12,10 +12,10 @@ template <class Category,
           class Pointer = T*, 
           class Reference = T&>
 struct iterator {
-    typedef Category iterator_category; 
-    typedef T value_type;
-    typedef Distance difference_type;
-    typedef Pointer pointer; 
+    typedef Category  iterator_category; 
+    typedef T         value_type;
+    typedef Distance  difference_type;
+    typedef Pointer   pointer; 
     typedef Reference reference;
 };
 
