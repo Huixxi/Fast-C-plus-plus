76880cadcff521467703bc0e8ce3dad5ac56b0cf
HU <huxixi.love.story@gmail.com>
2020-04-14 09:55:39 +0800

    Update stl_vector.h

--- Part X A Deep Understanding of STL/3. Sequence Containers/stl_vector.h
+++ Part X A Deep Understanding of STL/3. Sequence Containers/stl_vector.h
@@ -1,5 +1,5 @@
 // alloc is SGI STL's allocator
-template<class T, class Alloc = alloc>
+template <class T, class Alloc = alloc>
 class vector {
 public:
     // vector's nested types
@@ -87,3 +87,41 @@ protected:
         return result;
     }   
 }
+
+template <class T, class Alloc = alloc>
+void vector<T, Alloc>::insert_aux(iterator position, const T& x) {
+    if(finish != end_of_storage) {
+        construct(finish, *(finish - 1));
+        ++finish;
+        T x_copy = x;
+        copy_backward(position, finish - 2, finish - 1);
+        *position = x_copy;
+    }
+    else {
+        const size_type old_size = size();
+        const size_type len = old_size != 0 ? 2 * old_size : 1;
+        
+        iterator new_start = data_allocator::allocate(len);
+        iterator new_finish = new_start;
+        try {
+            new_finish = uninitialized_copy(start, position, new_start);
+            construct(new_finish, x);
+            ++new_finish;
+            new_finish = uninitialized_copy(position, finish, new_finish);
+        }
+        catch(...) {
+            // commit or rollback
+            destroy(new_start, new_finish);
+            data_allocator::decallocate(new_start, len);
+            throw;
+        }
+        
+        destroy(begin(), end());
+        deallocate();
+        
+        start = new_start;
+        finish = new_finish;
+        end_of_storage = new_start + len;
+    }
+}
+
